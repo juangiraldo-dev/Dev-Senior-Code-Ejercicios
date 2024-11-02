d1e072d94d6a60ecbad01dac63ee08b51f549d2c
Juan Giraldo <juandiego.2000@outlook.com>
2024-11-01 22:21:55 -0500

    Ejercicios Clase 01

---
+++ PedirPalabras.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PedirPalabras {
+    public static void main(String[] args) {
+
+        var scanner = new Scanner(System.in);
+
+        String palabra1, palabra2, palabra3;
+
+        System.out.println("Introduzca palabra 1: ");
+        palabra1 = scanner.nextLine();
+
+        System.out.println("Introduzca palabra 2: ");
+        palabra2 = scanner.nextLine();
+
+        System.out.println("Introduzca palabra 3: ");
+        palabra3 = scanner.nextLine();
+
+        System.out.printf("%s %s %s",palabra1,palabra2,palabra3);
+
+        scanner.close();
+
+    } 
+}
---
+++ SaludoPersonal.java
@@ -0,0 +1,5 @@
+public class SaludoPersonal {
+    public static void main(String[] args) {
+        System.out.println("Cesar, bienvenido al curso de Programación en Java!!!");
+    }
+}
